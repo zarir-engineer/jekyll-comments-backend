09bdd21147389c28d8fc3b24e608abb46c300861
Zarir Engineer <zarir.utube@gmail.com>
2025-03-22 17:52:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -118,6 +118,5 @@ app.post("/comments", (req, res) => {
 
 
 // Start server
-//app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
-app.listen(PORT, "::", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
 
